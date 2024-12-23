3d6d9468cecb7c4c5f27e469bfc91b2f45b38aca
Hiran George <141491815+herangeo@users.noreply.github.com>
2024-12-23 15:07:29 +0530

    Update settings.py

--- lehenga_collection/settings.py
+++ lehenga_collection/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for lehenga_collection project.
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Define the directory for collected static files
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
