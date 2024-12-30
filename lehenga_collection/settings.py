80e87634c42bbb5e04722f7436305c50b42db4ea
Hiran George <141491815+herangeo@users.noreply.github.com>
2024-12-30 17:27:13 +0530

    Update settings.py

--- lehenga_collection/settings.py
+++ lehenga_collection/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'products.context_processors.user_context'
+                'products.context_processors.user_context',
                 'products.context_processors.cart_count',
 
             ],
