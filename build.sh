0d9020ded7621ab26de0d40ec06f315c8317cb15
Hiran George <141491815+herangeo@users.noreply.github.com>
2025-01-07 16:06:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+
 if [[ $CREATE_SUPERUSER ]]; then
     python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" || echo "Superuser creation skipped or failed"
 fi
